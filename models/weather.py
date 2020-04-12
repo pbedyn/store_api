c3f91aa6cae11990a5f47acf66b038ace9cdd9fc
Pawel Bedynski <pawel.bedynski@gmail.com>
2020-04-12 18:43:05 +0200

    Update weather.py

--- models/weather.py
+++ models/weather.py
@@ -8,26 +8,31 @@ from db import db
 
 class WeatherModel(db.Model):
     __tablename__ = 'weather_data'
-      
-    def __init__(self, name):
-        self.name = name
     
-    ### returns json version of the name and price tuple
+    id = db.Column(db.Integer, primary_key = True)
+    country = db.Column(db.String(80))
+    location_name = db.Column(db.String(80))
+    date_hour = db.Column(db.String(80))
+    temp = db.Column(db.Float())
+    cloud = db.Column(db.String(80))
+    wind_dir = db.Column(db.String(80))
+    wind_speed = db.Column(db.Float())
+    gust_speed = db.Column(db.Float())
+    dry_h = db.Column(db.Float())
+    soar_h = db.Column(db.Float())
+    b_level_height = db.Column(db.Float())
+    freezing_level = db.Column(db.Float())
+    TI700 = db.Column(db.Float())
+    TI800 = db.Column(db.Float())
+    TI850 = db.Column(db.Float())
+    TI900 = db.Column(db.Float())
+    num_ti = db.Column(db.Integer())
+    lifted_index = db.Column(db.Float())
+    cape = db.Column(db.Float())
+    conv_inh = db.Column(db.Float())
+    helicity = db.Column(db.Float())
+    cloudwater = db.Column(db.Float())
+    cloudice = db.Column(db.Float())
+    
     def json(self):
-        return {'name': self.name, 'items': [item.json() for item in self.items.all()]}
-    
-    ### this is a class method to find item by name
-    def find_by_name(cls, name):
-        ### SELECT * FROM stores WHERE name = name LIMIT 1 i.e. returns first row
-        return cls.query.filter_by(name = name).first()
-    
-    ### this is a class method to upsert an item
-    def save_to_db(self):
-        ### save the model to the database
-        db.session.add(self)
-        db.session.commit()
-    
-    ### delete item from the database
-    def delete_from_db(self):
-        db.session.delete(self)
-        db.session.commit()
+        return self.json()
