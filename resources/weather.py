da8298a06946175658eb7084e5fd80ddfada016d
Pawel Bedynski <pawel.bedynski@gmail.com>
2020-04-12 17:56:15 +0200

    Add files via upload

---
+++ resources/weather.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from flask_restful import Resource
+from models.weather import WeatherModel
+
+class WeatherList(Resource):
+    def get(self):
+        return {'weather': [weather.json() for weather in WeatherModel.query.all()]}
