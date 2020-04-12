6fc0afcc748f03053b3a6d9386d87b00c27a1f48
Pawel Bedynski <pawel.bedynski@gmail.com>
2020-04-12 17:57:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from resources.weather import WeatherList
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
@@ -30,6 +31,7 @@ api.add_resource(Item, '/item/<string:name>') # http://127.0.0.1:5000/item/<name
 api.add_resource(StoreList, '/stores')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
+api.add_resource(WeatherList, '/weather')
 
 if __name__ == '__main__':
     from db import db
